256148d0b2e0a268a411f6d0aeebdd307f36bf0d
Gavin Szczesniak <Szczesg@ferris.edu>
2021-09-08 19:00:03 -0400

    Setting up Spawners:
    + Created empty game objects to be for the AIs to spawn in the game.
    + Created a cs script called Enemy_Spawner_1 to spawn in a type of enemy, more will be added to it to function.

---
+++ Top_Down_Game/Assets/Scripts/Enemy_Scripts/Enemy_Spawner_1.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Spawner_1 : MonoBehaviour {
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+}
